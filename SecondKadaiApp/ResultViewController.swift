880c98df002a9c415b37ce4f549f5b36788746d5
sk <16.simple.magic@gmail.com>
2021-02-03 21:36:43 +0900

    途中経過20210203

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 小峯彩結香 on 2021/02/03.
+//  Copyright © 2021 sayuka.komine. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    
+    // 2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
+       @IBOutlet weak var label: UILabel!
+       }
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+
+
+    @IBOutlet weak var label: UILabel!
+    @IBAction func send(_ sender: Any) {
+        label.text = "送る"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let ResultViewController:ResultViewController = segue.destination as! ResultViewController
+    }
+
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        // 他の画面から segue を使って戻ってきた時に呼ばれる
+    }
 
 }
 
