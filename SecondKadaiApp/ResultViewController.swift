32d23fa998eb897e0ec7c0b2c76c30905c9aa010
sk <16.simple.magic@gmail.com>
2021-02-06 17:05:37 +0900

    2/6 途中経過　起動して落ちる状態

--- SecondKadaiApp/ResultViewController.swift
+++ SecondKadaiApp/ResultViewController.swift
@@ -10,20 +10,18 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
-    
-    // 2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
-       @IBOutlet weak var label: UILabel!
-       }
+    var text : String?
     
     
+    @IBOutlet weak var label: UILabel!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    label.text = "こんにちは\(text!) "
         // Do any additional setup after loading the view.
     }
-    
+  
 
     /*
     // MARK: - Navigation
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,30 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
-    @IBOutlet weak var label: UILabel!
-    @IBAction func send(_ sender: Any) {
-        label.text = "送る"
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    @IBAction func unwind(_ segue: UIStoryboardSegue) {
-        
-    }
-
+    // @IBOutlet weak var textfield: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // segueから遷移先のResultViewControllerを取得する
-        let ResultViewController:ResultViewController = segue.destination as! ResultViewController
+        
+        
+        
+        let resultViewController: ResultViewController = segue.destination as! ResultViewController
+        resultViewController.text = textfield.text
     }
-
+    
+    @IBOutlet weak var textfield: UITextField!
+    
+    
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
         // 他の画面から segue を使って戻ってきた時に呼ばれる
     }
